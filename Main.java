4ce9d86601f486e6572f1fafa735009afc9e7341
cnguyen0691 <chaunguyen0691@gmail.com>
2019-04-25 16:25:35 -0400

    first commit

---
+++ Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Good day. What is your problem?" );
+        boolean ans = false;
+        while (ans == false) {
+                System.out.println("Enter your response here or Q to quit: ");
+                String answer = input.nextLine();
+                if (answer.equalsIgnoreCase("Q") ||
+                        answer.equals("i am feeling great")) {
+                    System.out.println(">>>End");
+                }
+
+            }
+        }
+    }
+
